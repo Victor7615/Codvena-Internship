d3cd15f5f01202556e202e43b0c3171ffe6ec303
Victor Emuchay <92019004+Victor7615@users.noreply.github.com>
2025-09-10 12:11:56 -0600

    Update Streamlit_Forcast_App_3.py

--- Volvo/Streamlit_Forcast_App_3.py
+++ Volvo/Streamlit_Forcast_App_3.py
@@ -49,7 +49,7 @@ def load_mlflow_model():
     # This bypasses the MLflow registry's hardcoded paths.
     # It assumes the mlruns folder is committed to the repo.
     # The path is constructed from your specific experiment and run ID.
-    model_path = "Volvo/mlruns/581584146196918816/models/m-265d8b5038ca4a2bb0ac092cccc63ed7/artifacts"
+    model_path = "Volvo/mlruns/581584146196918816/models/m-265d8b5038ca4a2bb0ac092cccc63ed7/artifacts/model.pkl"
     try:
         st.info(f"Attempting to load model from: {model_path}")
         # Load the model directly using the file path URI.
